980586653934abf2c18b000b8f705aa1b4a0ceb9
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-02-04 02:12:56 +0100

    Update Server.c

--- TCP_One_Client/Server.c
+++ TCP_One_Client/Server.c
@@ -5,67 +5,46 @@
 #include <arpa/inet.h>
 
 #define PORT 8080
-#define BUFFER_SIZE 1024
 
 int main() {
     int server_socket, client_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    char buffer[BUFFER_SIZE] = {0};
-    const char *message = "Hello from the Linux TCP Server!";
+    char buffer[1024] = {0};
 
-    // Create a socket
-    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Socket error");
+        return 1;
     }
 
-    // Configure server address
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    // Bind the socket to the server address
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Bind failed");
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1 ||
+        listen(server_socket, 3) == -1) {
+        perror("Bind/Listen error");
         close(server_socket);
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
-    // Listen for incoming connections
-    if (listen(server_socket, 3) == -1) {
-        perror("Listen failed");
+    printf("Listening on port %d...\n", PORT);
+
+    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_socket == -1) {
+        perror("Accept error");
         close(server_socket);
-        exit(EXIT_FAILURE);
+        return 1;
     }
 
-    printf("Server is listening on port %d...\n", PORT);
-
-    // Accept incoming connection
-    if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len)) == -1) {
-        perror("Accept failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    // Receive data from the client
-    if (recv(client_socket, buffer, BUFFER_SIZE, 0) == -1) {
-        perror("Receive failed");
-        close(client_socket);
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
+    recv(client_socket, buffer, sizeof(buffer), 0);
     printf("Client: %s\n", buffer);
 
-    // Send a response to the client
-    if (send(client_socket, message, strlen(message), 0) == -1) {
-        perror("Send failed");
-    }
-    printf("Message sent to client.\n");
+    send(client_socket, "Hello from server!", 18, 0);
 
-    // Close sockets
     close(client_socket);
     close(server_socket);
-
     return 0;
 }
+
