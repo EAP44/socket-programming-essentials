e07718e3ffb3068468211bf26f6e669fbc4022e0
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-02-16 09:18:41 +0100

    Update server.h

--- TCP_Mul_Client/server.h
+++ TCP_Mul_Client/server.h
@@ -4,7 +4,7 @@
 #define PORT 8800
 #define BUFFER_SIZE 2048
 #define MAX_CLIENTS 30
-#define WELCOME_MESSAGE "Welcome to the TCP Server!"
+#define WELCOME_MESSAGE "Welcome to the T-C-P Server!"
 
 void initialize_server(int *server_socket, struct sockaddr_in *server_addr);
 void handle_new_connection(int server_socket, int client_sockets[], fd_set *readfds, struct sockaddr_in *client_addr);
