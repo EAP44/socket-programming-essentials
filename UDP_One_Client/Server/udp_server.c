b828f2fa6e9f751158feca9b2e4c14523e01307f
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-05-12 12:23:37 +0100

    Update udp_server.c

--- UDP_One_Client/Server/udp_server.c
+++ UDP_One_Client/Server/udp_server.c
@@ -19,16 +19,16 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    // memset(&server_addr, 0, sizeof(server_addr));
+    // server_addr.sin_family = AF_INET;
+    // server_addr.sin_addr.s_addr = INADDR_ANY;
+    // server_addr.sin_port = htons(PORT);
 
-    if (bind(server_socket, (const struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Bind failed");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
+    // if (bind(server_socket, (const struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    //     perror("Bind failed");
+    //     close(server_socket);
+    //     exit(EXIT_FAILURE);
+    // }
 
     printf("UDP Server listening on port %d...\n", PORT);
 
